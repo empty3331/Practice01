4738b762e1fb210507149d19a88a4c0490d6ddd6
'Ju-Hye' <'empty3331@naver.com'>
2020-06-03 13:48:28 +0900

    문제 다시풀기

--- src/com/javaex/practice01/Ex14.java
+++ src/com/javaex/practice01/Ex14.java
@@ -9,14 +9,19 @@ public class Ex14 {
 		//직사각형 둘레와 면적 구하기
 		
 		Scanner sf = new Scanner(System.in);
+		double x,y;
+		
 								
 		System.out.print("가로를 입력하세요:");
-		double garo = sf.nextDouble();
+		x = sf.nextDouble();
 		System.out.print("세로를 입력하세요:");
-		double sero = sf.nextDouble();
+		y = sf.nextDouble();
 		
-		System.out.println("사각형의 넓이는 "+ garo*sero);
-		System.out.println("사각형의 둘레는 "+ (garo*2+sero*2));
+		double area = x*y;
+		double round = (x*2)+(y*2);
+		
+		System.out.println("사각형의 넓이는 "+ area);
+		System.out.println("사각형의 둘레는 "+ round);
 		sf.close();
 			
 
