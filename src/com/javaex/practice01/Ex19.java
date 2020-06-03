ea8ff87121c89418a31da337f68b517a0887a8c2
'Ju-Hye' <'empty3331@naver.com'>
2020-06-03 14:37:26 +0900

    문제 다시풀기

--- src/com/javaex/practice01/Ex19.java
+++ src/com/javaex/practice01/Ex19.java
@@ -10,8 +10,10 @@ public class Ex19 {
 	
 		long f = 300000;//초당 이동 거리
 		long g = 60*60*24*365;//1년 초단위로 계산
+		long year =1;
+		long yearkm = year*f*g;
 							
-		System.out.print("빛이 일년동안 가는 거리는"+f*g+" km 입니다.");
+		System.out.print("빛이 "+year+"년동안 가는 거리는"+yearkm+" km 입니다.");
 		
 		
 
