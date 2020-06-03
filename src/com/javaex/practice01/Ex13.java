7134e43af10903f74196f94502c1e18e7a925603
'Ju-Hye' <'empty3331@naver.com'>
2020-06-03 12:52:42 +0900

    문제 다시풀기

--- src/com/javaex/practice01/Ex13.java
+++ src/com/javaex/practice01/Ex13.java
@@ -10,12 +10,14 @@ public class Ex13 {
 		
 		final double dol =1230.95; //환율 상수로 고정
 						
-		Scanner se = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 						
 		System.out.print("환전할 원화를 입력하세요:");
-		double nummm = se.nextDouble();
-		System.out.print("받으실 달러는 "+nummm/dol);
-		se.close();
+		double won = sc.nextDouble();
+		double exch = won/dol;
+		
+		System.out.print("받으실 달러는 "+exch);
+		sc.close();
 		
 	}
 
